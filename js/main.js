f8c015ac25a179f4d9a9b6bd8c7080ddad876a0a
EfrainBM <145635254+EfrainBM@users.noreply.github.com>
2025-05-08 13:43:17 -0600

    Update main IP2.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const API_URL = "http://44.200.221.164:5000/api/devices";
+const API_URL = "http://44.199.198.188:5000/api/devices";
 const tableBody = document.getElementById("deviceTableBody");
 const statusActual = document.getElementById("statusActual");
 
