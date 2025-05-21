b9ee7f34a51049e08aa68941e4238b7b89dba397
EfrainBM <145635254+EfrainBM@users.noreply.github.com>
2025-05-21 11:31:39 -0600

    Update main9.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const API_URL = "http://34.205.166.69:5000/api/devices";
+const API_URL = "http://35.170.70.19:5000/api/devices";
 const tableBody = document.getElementById("deviceTableBody");
 const statusActual = document.getElementById("statusActual");
 
